6e8e3197b8f009eed65dc38c1839946f3ae4ef00
jgengo <jordane.gengo@gmail.com>
2025-01-04 19:12:27 +0200

    feat: add CI/CD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 const IBMPlexSans = localFont({
   variable: "--font-ibm-plex-sans",
   src: [
@@ -46,11 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${IBMPlexSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${IBMPlexSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
