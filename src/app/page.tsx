f746fda43edcb37cb1bd28bce4ace69bee0a4966
jgengo <jordane.gengo@gmail.com>
2025-01-04 19:35:36 +0200

    feat: add shacdn-ui and custom theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,7 @@ export default function Home() {
   return (
     <div>
       <p className="font-[family-name:var(--font-ibm-plex-sans)] text-4xl font-bold">
-        Hello World - Gekki
-      </p>
-      <p className="font-[family-name:var(--font-ibm-plex-sans)] text-4xl">
-        Hello World - Gekki
-      </p>
-      <p className="font-[family-name:var(--font-ibm-plex-sans)] text-4xl font-semibold">
-        Hello World - Gekki
+        Hello <span className="text-primary">World</span>
       </p>
     </div>
   );
