4df3999d131f5bcda534975389e7ec6ca75665ba
jgengo <jordane.gengo@gmail.com>
2025-01-11 12:10:11 +0200

    feat: implement PATCH api/tasks/:id

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -53,3 +53,69 @@ export async function DELETE(req, { params }) {
     );
   }
 }
+
+export async function PATCH(req, { params }) {
+  try {
+    const { id } = params;
+    if (!id) {
+      return NextResponse.json(
+        { error: "Task ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const session = await auth();
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const body = await req.json();
+    const { text, position, isCompleted } = body;
+
+    if (
+      text === undefined &&
+      position === undefined &&
+      isCompleted === undefined
+    ) {
+      return NextResponse.json(
+        { error: "No valid fields to update" },
+        { status: 400 }
+      );
+    }
+
+    await connectMongo();
+
+    const task = await Task.findById(id);
+
+    if (!task) {
+      return NextResponse.json({ error: "Task not found" }, { status: 404 });
+    }
+
+    const user = await User.findById(session.user.id);
+
+    // Allow updates if user owns the task or is admin
+    if (!user.tasks.includes(task._id) && !user.isAdmin) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const updateFields = {};
+    if (text !== undefined) updateFields.text = text;
+    if (position !== undefined) updateFields.position = position;
+    if (isCompleted !== undefined) updateFields.isCompleted = isCompleted;
+
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { $set: updateFields },
+      { new: true }
+    );
+
+    return NextResponse.json(updatedTask, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
