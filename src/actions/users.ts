6174b56f50d5365102a77136cf9b0b2d50aa1e5e
jgengo <jordane.gengo@gmail.com>
2025-01-18 23:32:03 +0200

    feat: improved security, performances (with lean()) and error handling. for getUsers()
    feat: simply return users
    
    chore: remove useless comments

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -1,15 +1,23 @@
 "use server";
 
+import { auth } from "@/auth";
 import connectMongo from "@/libs/mongoose";
 import User from "@/models/User";
 
 export async function getUsers() {
+    
     try {
+        const session = await auth();
+        if (!session?.user?.isAdmin) {
+            throw new Error("Unauthorized");
+        }
+
         await connectMongo();
-        const users = await User.find({});
+        const users = await User.find({}).lean();
+
         return users;
     } catch (error) {
-        console.error(error);
-        return [];
+        console.error("Failed to fetch users:", error);
+        throw new Error("Failed to fetch users");
     }
 }
